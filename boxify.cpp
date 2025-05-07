227adeda66046cf5fff616e5907876fef523e5fc
paulpeterkim <paulpeterkim@gmail.com>
2025-05-08 00:17:36 +0900

    feat: boxified files are saved in new directory

--- boxify.cpp
+++ boxify.cpp
@@ -2,7 +2,12 @@
 
 Boxifier::Boxifier(const std::string path)
     : path(path), fileReader(path) {
-    std::string line;
+    auto [name, ext] = splitExtension(path);
+    vector<string> parts = {BOXIFY_DIRECTORY + name.substr(2) + BOXIFY_INDICATOR, ext};
+    string outputFileName = join(parts, ".");
+    fileWriter.open(outputFileName);
+
+    string line;
     while (fileReader.readLine(line)) {
         trim(line);
         if (line.size() == 0)
@@ -10,41 +15,54 @@ Boxifier::Boxifier(const std::string path)
         if (line.size() > 2 && line[0] == '/' && line[1] == '/') {
             continue;
         }
-        // std::cout << "Adding line: " << line << NL;
         codeBlock.addLine(line);
     }
-    // codeBlock.print();
 }
 
-void Boxifier::boxify() {
+void Boxifier::boxifyAll() {
+    std::filesystem::create_directories(std::filesystem::path(BOXIFY_DIRECTORY).parent_path());
+    fileWriter.writeLine(BOXIFY_MACRO);
+    boxify(codeBlock);
+}
+
+void Boxifier::boxify(CodeBlock& block) {
     size_t si = 0;
-    std::vector lines = codeBlock.getLines();
-    for (size_t i = 0; i < codeBlock.getLinesSize(); ++i) {
-        if (codeBlock.getDepth()) {
-            if (i) {
-                std::cout << center_string(lines[i], codeBlock.getMaxLen() + 2) << '\n';
-            } else if (!i || lines[i].front() == '}') {
-                std::cout << center_string(lines[i], codeBlock.getMaxLen() + 2, '=') << '\n';
+    vector block_lines = block.getLines();
+    vector<unsigned int> lens = block.getMaxLenArr();
+
+    for (size_t i = 0; i < block.getLinesSize(); ++i) {
+        if (block.getDepth()) {
+            // Only make blocks if the depth of a code block is greater or equal to 1
+            string centered_line;
+            if (block_lines[i].front() != '}' && block_lines[i].back() == '{') {
+                // the start of a new code block
+                delete_curly_brackets(block_lines[i]);
+                trim(block_lines[i]);
+                centered_line = string(UPPERLEFT) + center_string(block_lines[i], block.getMaxLen() + 2, HORIZONTAL) + string(UPPERRIGHT);
+            } else if (block_lines[i].front() == '}' && block_lines[i].back() == '{') {  
+                // both the start and the end of a code block
+                delete_curly_brackets(block_lines[i]);
+                trim(block_lines[i]);
+                centered_line = string(VERTICALRIGHT) + center_string(block_lines[i], block.getMaxLen() + 2, HORIZONTAL) + string(VERTICALLEFT);
+            } else if (block_lines[i].front() == '}' && block_lines[i].back() != '{') {
+                // the end of a code block
+                delete_curly_brackets(block_lines[i]);
+                trim(block_lines[i]);
+                centered_line = string(LOWERLEFT) + center_string(block_lines[i], block.getMaxLen() + 2, HORIZONTAL) + string(LOWERRIGHT);
+            } else {
+                // Inside of a code block
+                centered_line = string(VERTICAL) + center_string(block_lines[i], block.getMaxLen() + 2) + string(VERTICAL);
             }
+            for (unsigned int i = block.getDepth() - 1; i > 0; --i) {
+                centered_line = center_string(centered_line, lens[i] + 2);
+                centered_line = string(VERTICAL) + centered_line + string(VERTICAL);
+            }
+            fileWriter.writeLine(centered_line);
+        } else {
+            fileWriter.writeLine(block_lines[i]);
         }
-        while (si < codeBlock.getSubBlocksPos().size() && codeBlock.getSubBlocksPos()[si] == i) {
-            codeBlock.getSubBlocks()[si++];
+        while (si < block.getSubBlocksPos().size() && block.getSubBlocksPos()[si] == i) {
+            boxify(*block.getSubBlocks()[si++]);
         } 
     }
-}
-
-int main() {
-    ExtensionFinder finder(EXTENSIONS);
-    
-    
-    // for (std::string path : finder.getFileNames()) {
-    //     std::cout << "Printing File: " << path << NL << LINE;
-    //     Boxifier boxifier(path);
-    //     std::cout << LINE;
-    // }
-    // std::cout << center_string("Hello World~", 14, '=') << NL;
-    Boxifier boxifier("./utils.hpp");
-    boxifier.boxify();
-
-    return 0; 
 }
--- boxify.hpp
+++ boxify.hpp
@@ -1,27 +1,43 @@
 #ifndef BOXIFY_HPP
 #define BIXIFY_HPP
 
-#include <iostream>
 #include <string>
+#include <iostream>
+#include <filesystem>
 
 #include "ExtensionFinder.hpp"
 #include "FileReader.hpp"
+#include "FileWriter.hpp"
 #include "CodeBlock.hpp"
 #include "utils.hpp"
 
 #define NL '\n'
 #define LINE "===========================================================\n"
 #define EXTENSIONS R"(\b\w+\.c\b|\b\w+\.cpp\b|\b\w+\.cc\b|\b\w+\.cxx\b|\b\w+\.h\b|\b\w+\.hpp\b)"
+#define VERTICAL u8"\u2551"
+#define HORIZONTAL u8"\u2550"
+#define UPPERLEFT u8"\u2554"
+#define UPPERRIGHT u8"\u2557"
+#define LOWERRIGHT u8"\u255D"
+#define LOWERLEFT u8"\u255A"
+#define VERTICALLEFT u8"\u2563"
+#define VERTICALRIGHT u8"\u2560"
 
+#define BOXIFY_MACRO "#define u8\"\u2551\" \n#define u8\"\u2550\" \n#define u8\"\u2554\" \n#define u8\"\u2557\" {\n#define u8\"\u255D\" \n#define u8\"\u255A\" }\n#define u8\"\u2563\" {\n#define u8\"\u2560\" }\n"
+#define BOXIFY_DIRECTORY "./boxified/"
+#define BOXIFY_INDICATOR "_boxified"
 class Boxifier {
 private:
     std::string path;
     FileReader fileReader;
+    FileWriter fileWriter;
     CodeBlock codeBlock;
+    vector<string> lines;
 
 public:
     Boxifier(const std::string path);
-    void boxify();
+    void boxifyAll();
+    void boxify(CodeBlock& block);
 };
     
 #endif
