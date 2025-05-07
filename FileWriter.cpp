4060b0eb816960bf8d2903ceb5396a2212b30ef7
paulpeterkim <paulpeterkim@gmail.com>
2025-05-07 23:59:56 +0900

    feat: create file writer class

---
+++ FileWriter.cpp
@@ -0,0 +1,32 @@
+#include "FileWriter.hpp"
+
+#include <iostream>
+FileWriter::FileWriter(){}
+
+FileWriter::FileWriter(const string& fileName) : fileName(fileName) {
+    outputFile.open(fileName);
+    if (!outputFile)
+        std::cerr << "Unable to write to file: " << fileName << '\n';
+}
+
+FileWriter::~FileWriter() {
+    if (outputFile.is_open())
+        outputFile.close();
+}
+
+bool FileWriter::open() {
+    outputFile.open(fileName);
+    if (!outputFile){
+        std::cerr << "Unable to write to file: " << fileName << '\n';
+        return false;
+    }
+    return true;
+}
+
+bool FileWriter::isOpen() const {
+    return outputFile.is_open();
+}
+
+void FileWriter::writeLine(string& line) {
+    outputFile << line << '\n';
+}
