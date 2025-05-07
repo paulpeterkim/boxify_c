309dfcec8af4fb4077356dc73b685134efeb6aeb
paulpeterkim <paulpeterkim@gmail.com>
2025-05-07 23:36:00 +0900

    feat: add length calculating function and fix padding function accordingly

--- utils.hpp
+++ utils.hpp
@@ -36,8 +36,46 @@ inline void trim(std::string &s) {
     ltrim(s);
 }
 
-inline std::string center_string(const std::string& text, unsigned int width, char pad = ' ') {
-    int len = text.length();
+inline void delete_curly_brackets(std::string &s) {
+    s.erase(
+        std::remove(
+            s.begin(),
+            s.end(),
+            '{'
+        ),
+        s.end()
+    );
+    s.erase(
+        std::remove(
+            s.begin(),
+            s.end(),
+            '}'
+        ),
+        s.end()
+    );
+}
+
+inline std::string delete_curly_brackets_copy(std::string s) {
+    delete_curly_brackets(s);
+    return s;
+}
+
+inline unsigned int visualLen(const std::string& s) {
+    unsigned int cnt = 0;
+    for (size_t i = 0; i < s.size();) {
+        unsigned char ch = s[i];
+        if (ch >> 7 == 0) ++i;
+        else if (ch >> 5 == 0x6) i += 2;
+        else if (ch >> 4 == 0xE) i += 3;
+        else if (ch >> 3 == 0x1E) i += 4;
+        else ++i;
+        ++cnt; 
+    }
+    return cnt;
+}
+
+inline std::string center_string(const std::string& text, unsigned int width, std::string pad = u8"\u0020") {
+    int len = visualLen(text);
     if (len >= width) {
         return text;
     }
@@ -47,13 +85,16 @@ inline std::string center_string(const std::string& text, unsigned int width, ch
     int right_pad = padding_len - left_pad;
 
     std::ostringstream result;
-    result << std::setfill(pad) << std::setw(left_pad) << "";
+    for (int i = 0; i < left_pad; ++i)
+        result << pad;
     
     result << text;
 
-    result << std::setfill(pad) << std::setw(right_pad) << "";
+    for (int i = 0; i < right_pad; ++i)
+        result << pad;
 
     return result.str();
 }
+
 
 #endif
